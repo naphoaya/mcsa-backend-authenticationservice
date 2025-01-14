f59db8444358b1b9cd092868b01770f84f98c364
naphoaya <naphoaya@metrosystems.co.th>
2025-01-14 12:13:54 +0700

    develop

---
+++ ConfigurationService/Controllers/v1/Test/TestController.cs
@@ -0,0 +1,37 @@
+﻿using MCSABackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MCSABackend.Controllers.V1.Test
+{
+
+    //[ApiController]
+    [ApiVersion("1.0")] // กำหนดเวอร์ชัน 1.0
+    [Route("api/v{version:apiVersion}/[controller]")]
+
+    public class TestController : ControllerBase
+    {
+        //private readonly ILogger<TestV1Controller> _logger;
+
+        private readonly ITestService _testService;
+       
+        public TestController(ITestService testService)
+        {
+            _testService=testService;
+        }
+
+        [HttpGet("CountryList")]
+        public IActionResult GetCourntryList()
+        {
+            var result= _testService.GetCountriesList();
+            return StatusCode(StatusCodes.Status200OK, result);
+            
+        }
+        [HttpGet("TestAPI")]
+        public IActionResult Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, "This is version 1.0");
+        }
+    }
+}
---
+++ ConfigurationService/Utilities/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace MCSABackend.Utilities
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; } // Indicates whether the operation was successful.
+        public string Message { get; set; } // A general message regarding the request (e.g., success or error description).
+        public T Data { get; set; } // The actual data, which is generic to handle various types.
+        public List<string> Errors { get; set; } // Any list of errors if applicable (e.g., validation errors).
+        public int StatusCode { get; set; } // The HTTP status code.
+
+        // Default constructor
+        public ApiResponse()
+        {
+            Errors = new List<string>();
+        }
+
+        // Parameterized constructor to set properties easily
+        public ApiResponse(bool success, string message, T data = default, List<string> errors = null, int statusCode = 200)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+            Errors = errors ?? new List<string>();
+            StatusCode = statusCode;
+        }
+    }
+
+}
